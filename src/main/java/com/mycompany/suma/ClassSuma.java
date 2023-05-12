06b9b853b0de57d1f0f9c3e24a81346704b0bc73
jriscos04 <123938737+jriscos04@users.noreply.github.com>
2023-05-12 10:26:31 +0200

    Add files via upload
    Primer métode

---
+++ src/main/java/com/mycompany/suma/ClassSuma.java
@@ -0,0 +1,20 @@
+package com.mycompany.suma;
+
+/**
+ *
+ * @author Jaavii
+ */
+public class ClassSuma {
+    
+    private int num1;
+
+    public ClassSuma(int num1) {
+        this.num1 = num1;
+    }
+    
+    public int sumar (int num1, int num2) {
+        int num = num1 + num2;
+        
+        return num;
+    }
+}
